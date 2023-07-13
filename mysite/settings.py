22d5919387ad13bc2c3a31eba5ce593f1cad04d0
Mehmet Ali Soylu <110969957+TurtleWithGlasses@users.noreply.github.com>
2023-07-13 20:34:01 +0300

    Update settings.py
    Removed email address & app password

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,5 +127,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "mhmtsoylu1928@gmail.com"
-EMAIL_HOST_PASSWORD = "wvzivxzqirwusksb"
+EMAIL_HOST_USER = "YOUR EMAIL ADDRESS"
+EMAIL_HOST_PASSWORD = "YOUR APP PASSWORD"
